019a4590124305a6e945570967606751c5cc37cd
Steven Jackson <steven@rootedlogics.com>
2016-06-28 13:19:22 -0400

    scripts

---
+++ ddump.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+project="empty"
+if [ -z "$1" ]
+  then
+    echo "No argument supplied, creating 'empty_'"
+  else
+    project=$1
+fi
+_now=$(date +"%m_%d_%Y_%H_%M")
+_file="/Users/stevenjackson/dumps/${project}_$_now.sql"
+echo "Starting backup to $_file..."
+drush sql-dump > "$_file"
