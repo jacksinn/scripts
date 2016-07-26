c7c8db76b1d901e78298b9b2c7f99c7c4c0fc832
Steven Jackson <steven@rootedlogics.com>
2016-07-26 12:10:52 -0400

    updated gap to remove outputs

---
+++ gapp.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+  "fmt"
+  "os"
+  "os/exec"
+  "log"
+)
+
+func main() {
+	arg := "Default"
+	if(len(os.Args) > 1){
+		arg = os.Args[1]
+	}
+	// fmt.Println(arg)
+	cmdGit := "git"
+	cmdArgs := []string{"add", "."}
+	tryExec(cmdGit, cmdArgs)
+
+	cmdArgs = []string{"commit", "-m", arg}
+	tryExec(cmdGit, cmdArgs)
+
+	cmdArgs = []string{"push"}
+	tryExec(cmdGit, cmdArgs)
+
+	// sha := string(out)
+	// fmt.Println(sha)
+}
+
+func tryExec(cmd []string, cmdArgs []string){
+		out, err := exec.Command(cmdGit, cmdArgs...).Output()
+	if(err != nil){
+		log.Fatal(err)
+	}
+}
